c76bb3e477a9f6f4f8b1a938f9ce945e1b4a1c55
seoe5580 <seoe5580@wrdsb.ca>
2020-09-15 19:04:40 +0000

    Finished the bigger number assignment

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Allows you to identify with number is bigger
+ * @author Eric Seo
  */
 public class Main {
 
@@ -9,7 +11,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // Asks the user to input a number
+    System.out.println("Please enter an integer:");
+
+    // gets the number from the user
+    int num1 = input.nextInt();
+
+    //Asks the user to input another number
+    System.out.println("Please enter another integer:");
+
+    // gets another number from the user
+    int num2 = input.nextInt();
+
+    if (num1 > num2){
+      System.out.println("The biggest number is " + num1);
+      } else {
+      System.out.println("The biggest number is " + num2);
+    }
     
   }
 }
